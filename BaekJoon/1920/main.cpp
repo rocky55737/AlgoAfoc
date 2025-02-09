f2b0dd0be8b534f2a082c2ae74dd09fc98a2c244
rocky55737 <rocky55737@gmail.com>
2025-02-09 10:57:46 +0900

    Fix 1920.md rocky55737 : amend the better code

--- BaekJoon/1920/main.cpp
+++ BaekJoon/1920/main.cpp
@@ -2,44 +2,32 @@
 #include <vector>
 #include <algorithm>
 
-int main() 
+int main()
 {
-  std::vector<int> a;
-  std::vector<int> v;
-  int n, m;
-  std::cin >> n;
-  for (int i = 0; i < n; i++) {
-    int temp;
-    std::cin >> temp;
-    a.push_back(temp);
-  }
-  std::sort(a.begin(), a.end());
-  std::cin >> m;
-  for (int i = 0; i < m; i++) {
-    int temp;
-    std::cin >> temp;
-    v.push_back(temp);
-  }
-  for (int i = 0; i < m; i++) {
-    int left = 0;
-    int right = n - 1;
-    int mid;
-    while (left <= right) {
-      mid = (left + right) / 2;
-      if (a[mid] == v[i]) {
-        std::cout << 1 << '\n';
-        break;
+    int N, M;
+    std::cin >> N;
+    std::vector<int> A(N);
+    for (int i = 0; i < N; i++)
+    {
+        std::cin >> A[i];
+    }
+    std::sort(A.begin(), A.end());
+    std::cin >> M;
+    std::vector<int> B(M);
+    for (int i = 0; i < M; i++)
+    {
+      std::cin >> B[i];
+    }
+    for (int i = 0; i < M; i++)
+    {
+      if (std::binary_search(A.begin(), A.end(), B[i]))
+      {
+        std::cout << "1\n";
       }
-      else if (a[mid] < v[i]) {
-        left = mid + 1;
-      }
-      else {
-        right = mid - 1;
+      else
+      {
+        std::cout << "0\n";
       }
     }
-    if (left > right) {
-      std::cout << 0 << '\n';
-    }
-  }
-  return 0;
+    return 0;
 }
